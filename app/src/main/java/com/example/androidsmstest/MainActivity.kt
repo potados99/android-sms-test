537e561a544c0337a4ea4b5e1dbd498f47cf3321
potados99 <potados99@gmail.com>
2019-02-02 04:05:44 +0900

    Default sms app change.

--- app/src/main/java/com/example/androidsmstest/MainActivity.kt
+++ app/src/main/java/com/example/androidsmstest/MainActivity.kt
@@ -1,12 +1,94 @@
 package com.example.androidsmstest
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val CHANGE_SMS_APP = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getPermissions()
+
+        var originSmsApp = getDefaultSmsApp()
+        updateCurrentSmsAppLabel(originSmsApp) // original sms app.
+        updateOriginSmsAppLabel(originSmsApp)
+
+
+        changeButton.setOnClickListener {
+            changeDefaultSmsApp(this.packageName)
+        }
+
+        revertButton.setOnClickListener {
+            changeDefaultSmsApp(originSmsApp)
+        }
+
+    }
+
+    private fun getDefaultSmsApp(): String {
+        return Telephony.Sms.getDefaultSmsPackage(this)
+    }
+
+    private fun updateOriginSmsAppLabel(text: String) {
+        originalSmsAppLabel.text = text
+    }
+
+    private fun updateCurrentSmsAppLabel(text: String) {
+        currentSmsAppLabel.text = text
+    }
+
+    private fun changeDefaultSmsApp(appName: String) {
+        val intent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
+        intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, appName)
+        startActivityForResult(intent, CHANGE_SMS_APP)
+    }
+
+    private fun getPermissions() {
+        requirePermission(Manifest.permission.RECEIVE_SMS)
+        requirePermission(Manifest.permission.READ_SMS)
+        requirePermission(Manifest.permission.SEND_SMS)
+        requirePermission(Manifest.permission.READ_CONTACTS)
+    }
+
+    private fun requirePermission(permission: String) {
+        val permissionCheck: Int = ContextCompat.checkSelfPermission(this@MainActivity, permission)
+        val granted = (permissionCheck == PackageManager.PERMISSION_GRANTED)
+
+        if (! granted) {
+            // if user denied permission.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                // Hey please give me permission :(
+                Toast.makeText(this@MainActivity, "Please allow access to $permission", Toast.LENGTH_SHORT).show()
+            }
+
+            // request.
+            ActivityCompat.requestPermissions(this, arrayOf(permission), 1)
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == CHANGE_SMS_APP) //check if the request code is the one you've sent
+        {
+            if (resultCode == RESULT_OK) {
+                updateCurrentSmsAppLabel(getDefaultSmsApp())
+            }
+            else {
+                Toast.makeText(this, "Failed to change default sms app.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
